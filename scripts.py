2780252dfecdac6d694b2a76cbcc824f9294d665
alee2187 <52175810+alee2187@users.noreply.github.com>
2020-03-13 20:16:45 -0400

    Updated

--- scripts.py
+++ scripts.py
@@ -13,8 +13,17 @@ def make_day_to_mdy(day):
 						day_to_mdy[i] = dt.date(2020,3,i-39).strftime("%m/%d/%Y")
 					except:
 						try:
-							day_to_mdy[i] = dt.date(2020,4,i-79).strftime("%m/%d/%Y")
+							day_to_mdy[i] = dt.date(2020,4,i-70).strftime("%m/%d/%Y")
 						except:
-							break
+							try: 
+								day_to_mdy[i] = dt.date(2020,5,i-100).strftime("%m/%d/%Y")
+							except:
+								try:
+									day_to_mdy[i] = dt.date(2020,6,i-131).strftime("%m/%d/%Y")
+								except:
+									break
 		mdy_to_day = dict(map(reversed,day_to_mdy.items()))
 		return day_to_mdy, mdy_to_day
+
+dtm,mtd = make_day_to_mdy(200)
+print(dtm)
